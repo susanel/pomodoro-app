b95e6d433a4003f9707a0bfd3bbad12d477f5bf4
Zuza <zuzannaloteria@gmail.com>
2024-11-10 22:06:25 +0100

    add favicon imports

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,10 @@ import duration from 'dayjs/plugin/duration';
 import { v4 as uuidv4 } from 'uuid';
 import { POMODORO_MODE, TASK_TYPE } from '../utils/constants';
 
+import svg_red from '../assets/favicon_red.svg';
+import svg_green from '../assets/favicon_green.svg';
+import svg_blue from '../assets/favicon_blue.svg';
+
 dayjs.extend(duration);
 
 export const defaultTasks = [
@@ -40,19 +44,19 @@ export const modes = [
     type: POMODORO_MODE.POMODORO,
     duration: dayjs.duration(25, 'minutes'),
     color: 'pomodoro.red',
-    faviconPath: '/src/assets/favicon_red.svg',
+    faviconPath: svg_red,
   },
   {
     type: POMODORO_MODE.SHORT_BREAK,
     duration: dayjs.duration(5, 'minutes'),
     color: 'pomodoro.green',
-    faviconPath: '/src/assets/favicon_green.svg',
+    faviconPath: svg_green,
   },
   {
     type: POMODORO_MODE.LONG_BREAK,
     duration: dayjs.duration(15, 'minutes'),
     color: 'pomodoro.blue',
-    faviconPath: '/src/assets/favicon_blue.svg',
+    faviconPath: svg_blue,
   },
 ];
 
