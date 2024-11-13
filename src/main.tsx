6a84bfceb2691b641c5f53780b563ebc48c8008e
Zuza <zuzannaloteria@gmail.com>
2024-11-13 11:06:08 +0100

    import change

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material';
 
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 
 let theme = createTheme({
