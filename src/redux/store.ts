4ec78ee0ea1eed905c890624c15a4c48bfeb36c1
Zuza <zuzannaloteria@gmail.com>
2025-02-16 21:26:25 +0100

    add store

---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import tasksReducer from '../pages/HomePage/TasksSlice';
+import timerReducer from '../pages/HomePage/TimerSlice';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer, // how to call it differentely so we don't have: state.tasks.tasks
+    timer: timerReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>
