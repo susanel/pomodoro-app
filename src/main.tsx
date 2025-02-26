a538eddfb491a5d398abc8d7485fc02bf326f6f3
Zuza <zuzannaloteria@gmail.com>
2025-02-27 00:00:28 +0100

    add comments

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./index.css";
 import { store } from "./redux/store";
 import { theme } from "./theme";
 
+// component rerenders in Strict Mode for React 18 -> data gets fetched twice!
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
@@ -18,3 +19,12 @@ createRoot(document.getElementById("root")!).render(
     </Provider>
   </StrictMode>
 );
+
+// createRoot(document.getElementById("root")!).render(
+//   <Provider store={store}>
+//     <ThemeProvider theme={theme}>
+//       <App />
+//       <TestComponent />
+//     </ThemeProvider>
+//   </Provider>
+// );
